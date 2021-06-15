80393b15162f4ce7c5efacc7e243dcb07cfa5e9b
2218675712 <weichengcheng@thewesthill.net>
2021-06-15 17:07:09 +0800

    学习在dva中调用接口

--- src/models/indexTest.js
+++ src/models/indexTest.js
@@ -1,12 +1,17 @@
+import * as apis from '../services/example'
+
 export default {
   namespace: 'indexTest',
   state: {
-    name: '小明'
+    name: '小明',
+    cnodeData:{}
   },
   reducers: {
     setName(state, payload) {
-
       return {...state, name: payload.data.name}
+    },
+    setcnodeData(state, payload) {
+      return {...state, cnodeData: payload.data}
     }
   },
   // 处理异步的
@@ -17,6 +22,18 @@ export default {
           name: '罗翔'
         }
       })
+    },
+    * testCnode({payLoad}, {put, call}){
+      // call是调用接口的
+      // 可以第二个参数放参数
+     let rel= yield call(apis.testCnode)
+      if (rel.data){
+        yield put({
+          // 如果有数据,可以进行赋值
+          type: 'setcnodeData', data: rel.data.data
+        })
+      }
+
     }
   }
 }
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'dva';
-
+import * as apis from '../services/example'
 const IndexPage = (props) => {
+  console.log(props)
   const handleSetName = () => {
     // 要先写命名空间在写函数
     props.dispatch({
@@ -20,11 +21,27 @@ const IndexPage = (props) => {
       }
     })
   }
+  // 页面调接口
+/*  useEffect(()=>{
+   apis.testCnode().then(res=>{
+     console.log(res)
+   })
+  },[])*/
+  const handleTestCnode = () => {
+    // 要先写命名空间在写函数
+    props.dispatch({
+      type: 'indexTest/testCnode',
+      data: {
+        name: '张三'
+      }
+    })
+  }
   return <div>
     <h1>我是首页</h1>
     {props.name}
     <button onClick={handleSetName}>setName</button>
     <button onClick={handleSetNameAsync}>setNameAsync</button>
+    <button onClick={handleTestCnode}>testCnode</button>
   </div>
 };
 
@@ -32,7 +49,8 @@ IndexPage.propTypes = {};
 const mapStateToProps = state => {
   return {
     msg: '珠海',
-    name: state.indexTest.name
+    name: state.indexTest.name,
+    cnodeData: state.indexTest.cnodeData
   }
 }
 export default connect(mapStateToProps)(IndexPage);
